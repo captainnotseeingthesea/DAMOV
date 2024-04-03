71770fdfb7d5dddc931e33175de8942bd84ecee3
xuanyi <xuanyi@xuanyi-virtual-machine.(none)>
2024-04-03 09:55:05 +0800

    fix debug bug

--- simulator/src/pin_cmd.cpp
+++ simulator/src/pin_cmd.cpp
@@ -55,8 +55,6 @@ PinCmd::PinCmd(Config* conf, const char* configFile, const char* outputDir, uint
     args.push_back(pinPath);
 
     //Global pin options
-    args.push_back("-injection");
-    args.push_back("child");
     
     args.push_back("-ifeellucky");
     args.push_back("-follow_execv"); //instrument child processes
